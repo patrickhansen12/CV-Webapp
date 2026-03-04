f338f8a350cf49f6d85f7da2c96dea179d2451d7
patrickhansen12 <mussi_kat@hotmail.com>
2026-03-05 00:28:20 +0100

    Updating the vite file: process.env.NODE_ENV === 'production' ? '/' : '/CV-Webapp/',

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import process from 'eslint-plugin-vue/lib/configs/base.js'
 
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/CV-Webapp/',
   plugins: [vue()],
   resolve: {
     alias: {
